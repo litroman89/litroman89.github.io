b2114ea19191630bf359b4132525993cf25bad68
Roman <romamedrep@gmail.com>
2021-04-07 19:13:20 +0300

    added shadow

--- jogajoga/js/main.js
+++ jogajoga/js/main.js
@@ -52,3 +52,58 @@ for (let smoothLink of smoothLinks) {
     });
 }
 
+// sending forms to email
+
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+    const message = {
+        loading: 'Loading...',
+        success: 'Message sent',
+        failure: 'Something went wrong...'
+    };
+
+    const postData = async (url, data) => {
+        // document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            // item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    // statusMessage.textContent = message.success;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+forms();
